874b6d03d09f867381aa094823ac30fdb1321715
be-desarrollo <desarrollo@bigelephant.mx>
2018-10-18 17:42:53 -0500

    U[VISTA] Se actualizo la vista de Quinta Generacion, y se agrego el bootstrap 3.7, tambien se completo la hoja de mensajeEnvio y la de Contacto

---
+++ contacto.php
@@ -0,0 +1,28 @@
+<?php
+
+// ----------- LLAMAR CAMPOS -----------
+
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$asuntocliente = $_POST ['asunto'];
+$mensaje = $_POST['mensaje'];
+
+
+// ----------- DATOS PARA EL CORREO -----------
+
+$destinatario = "";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Asunto: $asuntocliente  \n";
+$carta .= "E-mail: $email \n";
+$carta .= "Mensaje: $mensaje";
+
+// ----------- ENVIO DE MENSAJE -----------
+
+mail($destinatario, $asunto, $carta);
+header('Location:mensajeEnvio.html'); // NUEVA HOJA DE MENSAJE ENVIADO
+
+ ?>
